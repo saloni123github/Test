3e3bec465146784397ce2ba30562ec6b261f7096
saloni <salonikolhe891@gmail.com>
2022-09-20 14:15:31 +0530

    first commit

---
+++ ArrayListTest/src/com/saloni/ArrayList1.java
@@ -0,0 +1,22 @@
+package com.saloni;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class ArrayList1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<String> list =new ArrayList<String>();
+		list.add("saloni");
+		list.add("tejal");
+		list.add("kasim");
+		list.add("pavan");
+		Iterator<String> itr = list.iterator();
+		while(itr.hasNext()) {
+			System.out.println(itr.next());
+		}
+
+	}
+
+}
